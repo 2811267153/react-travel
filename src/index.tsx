f76543a8a01ca8fa213ab2e31caa2cf2de66d419
xuzesen <2811267153@qq.com>
2023-02-20 20:51:46 +0800

    使用axios向后台发送数据

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import {BrowserRouter} from "react-router-dom";
 import "./i18n/config"
 import {Provider} from "react-redux";
 import store from "./store/store";
+import axios from "axios";
+
+axios.defaults.headers["x-icode"] ="943C78160B37B45C"
 const container = document.getElementById('root')!;
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,59 @@
 import {CarouselS, Header, ProductCollection, SideMenu} from "../../components";
-import { Col, Row, Typography} from "antd";
-import {productList1, productList2, productList3} from "../../components/common/mockup";
+import {Col, Row, Typography} from "antd";
 import sideImage1 from "../../assets/image/sider_2019_12-09.png";
 import React from "react";
-import {withRouter} from "../../helper/withRouter";
 import {withTranslation, WithTranslation} from "react-i18next";
+import axios from "axios";
+import {productList1, productList2} from "../../components/common/mockup";
+import {Spin} from "antd/lib";
 
-class HomePageComponent extends React.Component<WithTranslation> {
+//
+interface State {
+    productList: any[],
+    loading: boolean,
+    error: string | null
+}
+
+class HomePageComponent extends React.Component<WithTranslation, State> {
+
+    //初始化函数 把产品列表初始化为空数组
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            productList: [],
+            loading: true,
+            error: null,
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const {data} = await axios.get("http://123.56.149.216:8080/api/productCollections")
+            this.setState({
+                productList: data,
+                loading: false,
+                error: null
+            })
+        }catch (e) {
+            if (e instanceof Error) {
+                this.setState({
+                    loading: false,
+                    error: e.message
+                })
+            }
+        }
+    }
+
     render() {
         const {t} = this.props
+        const {productList, loading, error} = this.state
+        if(loading) {
+            return <Spin size="large"></Spin>
+        }
+
+        if(error) {
+            return  <>网站出错: {error}</>
+        }
         return (
             <div className="page-content w">
                 <Header/>
@@ -21,13 +66,16 @@ class HomePageComponent extends React.Component<WithTranslation> {
                             <CarouselS></CarouselS>
                         </Col>
                     </Row>
-                    <ProductCollection title={<Typography.Title level={3} type="warning">{t("home_page.hot_recommended")} </Typography.Title>}
-                                       products={productList1} sideImage={sideImage1}></ProductCollection>
-                    <ProductCollection title={<Typography.Title level={3} type="danger"> {t("home_page.new_arrival")} </Typography.Title>}
-                                       products={productList2} sideImage={sideImage1}></ProductCollection>
+                    <ProductCollection title={<Typography.Title level={3}
+                                                                type="warning">{t("home_page.hot_recommended")} </Typography.Title>}
+                                       products={productList[0].touristRoutes} sideImage={sideImage1}></ProductCollection>
+                    <ProductCollection title={<Typography.Title level={3}
+                                                                type="danger"> {t("home_page.new_arrival")} </Typography.Title>}
+                                       products={productList[1].touristRoutes} sideImage={sideImage1}></ProductCollection>
                     <ProductCollection
-                        title={<Typography.Title level={3} type="success"> {t("home_page.domestic_travel")} </Typography.Title>}
-                        products={productList3} sideImage={sideImage1}></ProductCollection>
+                        title={<Typography.Title level={3}
+                                                 type="success"> {t("home_page.domestic_travel")} </Typography.Title>}
+                        products={productList[2].touristRoutes} sideImage={sideImage1}></ProductCollection>
                 </div>
             </div>
         )
