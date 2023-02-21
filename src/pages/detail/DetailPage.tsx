e81da08505a944c8261d59a5124d04ecf1b45cd8
xuzesen <2811267153@qq.com>
2023-02-21 21:47:26 +0800

    页面框架详情与日期选择搭建

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,12 +1,62 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-
+import axios from "axios";
+import {Header} from "../../components";
+import {Col, DatePicker, Row, Spin, Space} from "antd";
+import {ProductIntro} from "../../components";
 
 export const DetailPage: React.FC = () => {
     const props = useParams<"touristRouteId">()
+    const [loading, setLoading] = useState<boolean>(true)
+    const [product, setProducr] = useState<any>(null)
+    const [error, setError] = useState<string | null>(null)
+
+    const {RangePicker} = DatePicker;
+    console.log(props)
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            try {
+                const {data} = await axios.get(`http://123.56.149.216:8080/api/touristRoutes/${props.touristRouteId}`)
+                setProducr(data)
+                console.log(data)
+                setLoading(false)
+                setError(null)
+            } catch (e) {
+                setError(e instanceof Error ? e.message : "error")
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [])
+    if (loading) {
+        return <Spin size="large"></Spin>
+    }
+
+    if (error) {
+        return <>网站出错: {error}</>
+    }
     return (
+
         <div>
-            路由路线详情页面, 路由ID: {props.touristRouteId}
+            <Header/>
+            <div className="page-content w">
+                <>
+                    <Row>
+                        <Col span={13}>
+                            <ProductIntro coupons={product.coupons} discount={product.discount}
+                                          touristRoutePictures={product.touristRoutePictures} price={product.price} rating={product.rating}
+                                          shortDescription={product.shortDescription}
+                                          title={product.title}></ProductIntro>
+                        </Col>
+                        <Col span={11}>
+                            <Space direction="vertical" size={12}>
+                                <RangePicker style={{marginTop: "40px"}}/>
+                            </Space>
+                        </Col>
+                    </Row>
+                </>
+            </div>
         </div>
     )
 }
