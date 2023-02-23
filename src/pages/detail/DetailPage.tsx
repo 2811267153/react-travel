c566cda20760f6b6bbe6455fa1e475a84326f413
xuzesen <2811267153@qq.com>
2023-02-23 21:51:37 +0800

    详情页面添加锚点组件

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import {Header, ProductIntro, ProductComments} from "../../components";
-import {Col, DatePicker, Row, Spin, Space, Divider, Typography} from "antd";
+import {Col, DatePicker, Row, Spin, Space, Divider, Typography, Anchor, Menu} from "antd";
 import {commentMockData} from "../../components/ProductComments/mockup";
 
 export const DetailPage: React.FC = () => {
@@ -44,7 +44,8 @@ export const DetailPage: React.FC = () => {
                     <Row>
                         <Col span={13}>
                             <ProductIntro coupons={product.coupons} discount={product.discount}
-                                          touristRoutePictures={product.touristRoutePictures} price={product.price} rating={product.rating}
+                                          touristRoutePictures={product.touristRoutePictures} price={product.price}
+                                          rating={product.rating}
                                           shortDescription={product.shortDescription}
                                           title={product.title}></ProductIntro>
                         </Col>
@@ -55,25 +56,44 @@ export const DetailPage: React.FC = () => {
                         </Col>
                     </Row>
                 </>
-                <Divider orientation={"center"}>
-                    <Typography.Title level={3}>产品特色</Typography.Title>
-                </Divider>
-                <div dangerouslySetInnerHTML={{__html:product.features}} style={{marginTop: 50}}></div>
-
-                <div style={{marginTop: 40}}>
+                <Anchor className="Anchor" direction="horizontal"
+                    items={[
+                    {
+                        key: '1',
+                        href: '#feature',
+                        title: 'Part 1',
+                    },         {
+                        key: '2',
+                        href: '#fees',
+                        title: 'Part 2',
+                    }, {
+                            key: '3',
+                            href: '#notes',
+                            title: 'Part 3',
+                        },
+                ]}>
+                </Anchor>
+                <div id="feature">
                     <Divider orientation={"center"}>
-                            <Typography.Title level={3}>费用</Typography.Title>
+                        <Typography.Title level={3}>产品特色</Typography.Title>
                     </Divider>
-                    <div dangerouslySetInnerHTML={{__html:product.fees}} style={{marginTop: 50}}></div>
+                    <div dangerouslySetInnerHTML={{__html: product.features}} style={{marginTop: 50}}></div>
                 </div>
-                <div style={{marginTop: 40}}>
+
+                <div style={{marginTop: 40}} id="fees">
                     <Divider orientation={"center"}>
                         <Typography.Title level={3}>费用</Typography.Title>
                     </Divider>
-                    <div dangerouslySetInnerHTML={{__html:product.notes}} style={{marginTop: 50}}></div>
+                    <div dangerouslySetInnerHTML={{__html: product.fees}} style={{marginTop: 50}}></div>
+                </div>
+                <div style={{marginTop: 40}} id="notes">
+                    <Divider orientation={"center"}>
+                        <Typography.Title level={3}>费用</Typography.Title>
+                    </Divider>
+                    <div dangerouslySetInnerHTML={{__html: product.notes}} style={{marginTop: 50}}></div>
                 </div>
                 <Divider plain>
-                        <Typography.Title level={3}>产品特色</Typography.Title>
+                    <Typography.Title level={3}>产品特色</Typography.Title>
                 </Divider>
                 <ProductComments data={commentMockData}></ProductComments>
 
